77bd517e9e8aa454e5eff146a6f93202a60d80e2
Kizyu08 <forutexx08@gmail.com>
2018-08-23 10:16:42 +0900

    モデル追加1弾

---
+++ Assets/scripts/LogSave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class LogSave : MonoBehaviour
+{
+    public void logSave(string txt)
+    {
+        StreamWriter sw;
+        FileInfo fi;
+        fi = new FileInfo(Application.dataPath + "/logfile.txt");
+        sw = fi.AppendText();
+        sw.WriteLine(txt);
+        sw.Flush();
+        sw.Close();
+    }
+}
